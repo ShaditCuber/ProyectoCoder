33c75c111ab8145680e9a4bbcc697cf7b539c0dd
FelipeIgnacio <felipebastidas2013@gmail.com>
2022-07-04 23:58:40 -0400

    Templates Nuevos

--- AppCoder/views.py
+++ AppCoder/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 #from django.shortcuts import render
 from AppCoder.models import Curso
 # Create your views here.
@@ -9,3 +10,34 @@ def curso(self):
     curso.save()
     documento_de_texto=f"   --->Curso : {curso.nombre} \n Comision: {curso.comision}"
     return HttpResponse(documento_de_texto)
+
+
+def inicio(request):
+    """ return HttpResponse("Vista de inicio") """
+    return render(request, 'AppCoder/inicio.html')
+
+def cursos(request):
+   """  return HttpResponse("Vista de cursos") """
+   return render(request, 'AppCoder/cursos.html')
+
+def estudiantes(request):
+    """ return HttpResponse("Vista de estudiantes") """
+    return render(request, 'AppCoder/estudiantes.html')
+
+def profesores(request):
+    """ return HttpResponse("Vista de profesores")  """  
+    return render(request, 'AppCoder/profesores.html')
+
+def entregables(request):
+    """ return HttpResponse("Vista de entregables") """
+    return render(request, 'AppCoder/entregables.html')
+
+'''def cursos(self):
+    cursos = Curso.objects.all()
+    documento_de_texto=""
+    for curso in cursos:
+        documento_de_texto+=f"   --->Curso : {curso.nombre} \n Comision: {curso.comision} \n"
+    return HttpResponse(documento_de_texto)'''
+
+
+
