8d1e176b3e1063e61a4d8f63d2b834d1376e85cc
FelipeIgnacio <felipebastidas2013@gmail.com>
2022-07-08 23:31:41 -0400

    Busqueda , Creacion de Objetos . Mejorar busqueda en una plantilla

--- AppCoder/urls.py
+++ AppCoder/urls.py
@@ -3,10 +3,16 @@ from .views import  *
 
 
 urlpatterns = [
-        path('curso/', curso),
+        #path('curso/', curso),
         #name sirve para llamar a la vista sin url
         path('', inicio, name='inicio'),
         path('cursos/', cursos, name='cursos'),
         path('estudiantes/', estudiantes,name='estudiantes'),
         path('profesores/', profesores,name='profesores'),
-        path('entregables/', entregables,name='entregables'),]
+        path('entregables/', entregables,name='entregables'),
+        #path('cursoFormulario/', cursoFormulario,name='cursoFormulario'),
+        #path('profeFormulario/', profeFormulario,name='profeFormulario'),
+        path('buscarComision/', buscarComision,name='buscarComision'),
+        path('buscar/', buscar,name='buscar'),
+        
+        ]
--- AppCoder/views.py
+++ AppCoder/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 #from django.shortcuts import render
-from AppCoder.models import Curso
+from AppCoder.models import Curso,Profesor
+from AppCoder.forms import CursoFormulario, ProfesorFormulario
 # Create your views here.
 
 
@@ -17,16 +18,41 @@ def inicio(request):
     return render(request, 'AppCoder/inicio.html')
 
 def cursos(request):
-   """  return HttpResponse("Vista de cursos") """
-   return render(request, 'AppCoder/cursos.html')
+    if request.method == 'POST':
+
+        miFormulario = CursoFormulario(request.POST)
+        if miFormulario.is_valid():
+            informacion=miFormulario.cleaned_data
+            curso = Curso(nombre=informacion['nombre'], comision=informacion['comision'])
+            curso.save()
+            return render(request, 'AppCoder/inicio.html')
+    else:
+        miFormulario = CursoFormulario()
+
+    return render(request, 'AppCoder/cursos.html',{"miFormulario":miFormulario})
+  # return HttpResponse("Vista de cursos") 
+   #return render(request, 'AppCoder/cursos.html')
 
 def estudiantes(request):
     """ return HttpResponse("Vista de estudiantes") """
     return render(request, 'AppCoder/estudiantes.html')
 
 def profesores(request):
-    """ return HttpResponse("Vista de profesores")  """  
-    return render(request, 'AppCoder/profesores.html')
+    #return HttpResponse("Vista de profesores")   
+    #return render(request, 'AppCoder/profesores.html')
+    if request.method == 'POST':
+
+        miFormulario = ProfesorFormulario(request.POST)
+        if miFormulario.is_valid():
+            informacion=miFormulario.cleaned_data
+            profesor = Profesor(nombre=informacion['nombre'], apellido=informacion['apellido'], 
+                                            email=informacion['email'], profesion=informacion['profesion'])
+            profesor.save()
+            return render(request, 'AppCoder/inicio.html')
+    else:
+        miFormulario = ProfesorFormulario()
+
+    return render(request, 'AppCoder/profesores.html',{"miFormulario":miFormulario})
 
 def entregables(request):
     """ return HttpResponse("Vista de entregables") """
@@ -40,4 +66,63 @@ def entregables(request):
     return HttpResponse(documento_de_texto)'''
 
 
+#VISTA FORMULARIO HTML
+""" def cursoFormulario(request):
 
+    if request.method == 'POST':
+
+        nombre = request.POST['curso']
+        comision = request.POST['comision']
+        curso = Curso(nombre=nombre, comision=comision)
+
+        curso.save()
+        return render(request, 'AppCoder/inicio.html')
+
+
+    return render(request, 'AppCoder/cursoFormulario.html') """
+
+#VISTA DE FORMS DJANGO
+""" def cursoFormulario(request):
+
+    if request.method == 'POST':
+
+        miFormulario = CursoFormulario(request.POST)
+        if miFormulario.is_valid():
+            informacion=miFormulario.cleaned_data
+            curso = Curso(nombre=informacion['nombre'], comision=informacion['comision'])
+            curso.save()
+            return render(request, 'AppCoder/inicio.html')
+    else:
+        miFormulario = CursoFormulario()
+
+    return render(request, 'AppCoder/cursoFormulario.html',{"miFormulario":miFormulario}) """
+
+""" 
+def profeFormulario(request):
+
+    if request.method == 'POST':
+
+        miFormulario = ProfesorFormulario(request.POST)
+        if miFormulario.is_valid():
+            informacion=miFormulario.cleaned_data
+            profesor = Profesor(nombre=informacion['nombre'], apellido=informacion['apellido'], 
+                                            email=informacion['email'], profesion=informacion['profesion'])
+            profesor.save()
+            return render(request, 'AppCoder/inicio.html')
+    else:
+        miFormulario = ProfesorFormulario()
+
+    return render(request, 'AppCoder/profeFormulario.html',{"miFormulario":miFormulario}) """
+
+
+def buscarComision(request):
+    return render(request, 'AppCoder/buscarComision.html')
+
+def buscar(request):
+    respuesta = f"Estoy buscando la camada nro: {request.GET['comision']}"
+    comision=request.GET['comision']
+    if comision!="":
+        cursos = Curso.objects.filter(comision=comision)
+        return render(request, 'AppCoder/resultadosBusqueda.html',{'cursos':cursos, 'comision':comision})
+    else:
+         return render(request, 'AppCoder/buscarComision.html',{"error":"No se ingreso una comision"})
