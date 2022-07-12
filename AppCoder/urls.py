ebaa4899505ae6f8ac4c386399a2c14bae7bd8fb
FelipeIgnacio <felipebastidas2013@gmail.com>
2022-07-12 19:53:33 -0400

    CRUD y Vistas de Clases

--- AppCoder/urls.py
+++ AppCoder/urls.py
@@ -17,4 +17,21 @@ urlpatterns = [
         path('imagenes/', imagenes,name='imagenes'),
         path('buscarImagen/', buscarImagen,name='buscarImagen'),
         path('buscarImagens/', buscarImagens,name='buscarImagens'),
+        path('leerProfesores/', leerProfesores,name='leerProfesores'),
+        path('eliminarProfesor/<nombre_profesor>', eliminarProfesor,name='eliminarProfesor'),
+        path('editarProfesor/<nombre_profesor>', editarProfesor,name='editarProfesor'),
+
+
+        #url con CBV
+        path('estudiante/list/', EstudianteList.as_view(), name='estudiante_list'),
+        path('estudiante/<pk>', EstudianteDetalle.as_view(), name='estudiante_detalle'),
+        path('estudiante/new/', EstudianteCreacion.as_view(), name='estudiante_new'),
+        path('estudiante/editar/<pk>', EstudianteEdicion.as_view(), name='estudiante_edit'),
+        path('estudiante/eliminar/<pk>', EstudianteEliminar.as_view(), name='estudiante_delete'),
+
+
+        path('buscarProfesor/', buscarProfesor,name='buscarProfesor'),
+        path('buscandoProfesor/', buscandoProfesor,name='buscandoProfesor'),
+
+
         ]
--- AppCoder/views.py
+++ AppCoder/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 #from django.shortcuts import render
-from AppCoder.models import Curso,Profesor,Imagenes
+from AppCoder.models import Curso,Profesor,Imagenes,Estudiante
 from AppCoder.forms import CursoFormulario, ImagenFormulario, ProfesorFormulario
+from django.views.generic import ListView,DetailView,CreateView,UpdateView,DeleteView
+from django.urls import reverse_lazy
 # Create your views here.1
 
 
@@ -39,22 +41,7 @@ def estudiantes(request):
     """ return HttpResponse("Vista de estudiantes") """
     return render(request, 'AppCoder/estudiantes.html')
 
-def profesores(request):
-    #return HttpResponse("Vista de profesores")   
-    #return render(request, 'AppCoder/profesores.html')
-    if request.method == 'POST':
 
-        miFormulario = ProfesorFormulario(request.POST)
-        if miFormulario.is_valid():
-            informacion=miFormulario.cleaned_data
-            profesor = Profesor(nombre=informacion['nombre'], apellido=informacion['apellido'], 
-                                            email=informacion['email'], profesion=informacion['profesion'])
-            profesor.save()
-            return render(request, 'AppCoder/inicio.html')
-    else:
-        miFormulario = ProfesorFormulario()
-
-    return render(request, 'AppCoder/profesores.html',{"miFormulario":miFormulario})
 
 def entregables(request):
     """ return HttpResponse("Vista de entregables") """
@@ -158,5 +145,96 @@ def buscarImagens(request):
 
 def buscarImagen(request):
     return render(request, 'AppCoder/buscarImagen.html')
+
+
+#CRUD DE PROFESORES
+# buscar profesor especifico
+#def buscarProfesor(request):
+def buscarProfesor(request):
+    return render(request, 'AppCoder/buscarProfesor.html')
+
+def buscandoProfesor(request):
+    #respuesta = f"Estoy buscando la camada nro: {request.GET['comision']}"
+    nombre=request.GET['nombre']
+    if nombre!="":
+        profesor = Profesor.objects.filter(nombre=nombre)
+        return render(request, 'AppCoder/buscarProfesor.html',{'profesor':profesor, 'nombre':nombre})
+    else:
+         return render(request, 'AppCoder/buscarProfesor.html',{"error":"No se ingreso un profesor"})
+
+def profesores(request):
+    #return HttpResponse("Vista de profesores")   
+    #return render(request, 'AppCoder/profesores.html')
+    if request.method == 'POST':
+
+        miFormulario = ProfesorFormulario(request.POST)
+        if miFormulario.is_valid():
+            informacion=miFormulario.cleaned_data
+            profesor = Profesor(nombre=informacion['nombre'], apellido=informacion['apellido'], 
+                                            email=informacion['email'], profesion=informacion['profesion'])
+            profesor.save()
+            profesores=Profesor.objects.all()
+            return render(request, 'AppCoder/leerProfesores.html',{'profesores':profesores})
+    else:
+        miFormulario = ProfesorFormulario()
+
+    return render(request, 'AppCoder/profesores.html',{"miFormulario":miFormulario})
+
+def leerProfesores(request):
+    profesores=Profesor.objects.all()
+    return render(request, 'AppCoder/leerProfesores.html',{'profesores':profesores})
+
+#ver esto
+def eliminarProfesor(request,nombre_profesor):
+    profesor = Profesor.objects.get(nombre=nombre_profesor)
+    profesor.delete()
+
+    profesores=Profesor.objects.all()
+    return render(request, 'AppCoder/leerProfesores.html',{'profesores':profesores})
+
+def editarProfesor(request,nombre_profesor):
     
+    profesor = Profesor.objects.get(nombre=nombre_profesor)
+    if request.method == 'POST':
+
+        miFormulario = ProfesorFormulario(request.POST)
+        if miFormulario.is_valid():
+            informacion=miFormulario.cleaned_data
+            profesor.nombre=informacion['nombre']
+            profesor.apellido=informacion['apellido']
+            profesor.email=informacion['email']
+            profesor.profesion=informacion['profesion']
+            profesor.save()
+
+            profesores=Profesor.objects.all()
+            return render(request, 'AppCoder/leerProfesores.html',{'profesores':profesores})
+    else:
+        miFormulario = ProfesorFormulario(initial={'nombre':profesor.nombre, 'apellido':profesor.apellido, 'email':profesor.email, 'profesion':profesor.profesion})
+
+    return render(request, 'AppCoder/editarProfesor.html',{"miFormulario":miFormulario,"nombre_profesor":nombre_profesor})
+
+
+#CRUD DE ESTUDIANTES , VISTAS BASADAS EN CLASES
+
+class EstudianteList(ListView):
+    model = Estudiante
+    template_name = 'AppCoder/estudiantes_list.html'
+     
+
+class EstudianteDetalle(DetailView):
+    model = Estudiante
+    template_name = 'AppCoder/estudiantes_detalle.html'
     
+class EstudianteCreacion(CreateView):
+    model = Estudiante
+    success_url = reverse_lazy('estudiante_list')
+    fields = ['nombre', 'apellido' ]
+
+class EstudianteEdicion(UpdateView):
+    model = Estudiante
+    success_url = reverse_lazy('estudiante_list')
+    fields = ['nombre', 'apellido' ]
+
+class EstudianteEliminar(DeleteView):
+    model = Estudiante
+    success_url = reverse_lazy('estudiante_list')
